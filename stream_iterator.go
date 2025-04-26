7f1143b7073c0453e79fb92db5666acd34334db6
Amit Lieberman <amit.lieberman@solaredge.com>
2025-04-26 07:22:16 +0300

    Support indexed Iterator

--- stream_iterator.go
+++ stream_iterator.go
@@ -7,3 +7,14 @@ func (s Stream[T]) Iterator(yield func(T) bool) {
 	}).FindFirst().
 		MustGetOptional()
 }
+
+func (s Stream[T]) IndexedIterator(yield func(int, T) bool) {
+	// Use a counter to keep track of the index
+	index := -1
+	s.Filter(func(v T) bool {
+		index++
+		// Yield return false if we need to stop (e.g. break within the loop)
+		return !yield(index, v)
+	}).FindFirst().
+		MustGetOptional()
+}
