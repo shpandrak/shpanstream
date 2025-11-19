c6f4cc16774926e5cc66f948e07be9e5968f4ea3
Amit Lieberman <amit.lieberman@solaredge.com>
2025-11-19 15:00:13 +0200

    Refactor openapi package to support datasoruce.Datasoruce rather then report.Datasource

--- utils/timeseries/tsquery/queryopenapi/openapi_parser_datasource.go
+++ utils/timeseries/tsquery/queryopenapi/openapi_parser_datasource.go
@@ -6,87 +6,60 @@ import (
 	"github.com/shpandrak/shpanstream/stream"
 	"github.com/shpandrak/shpanstream/utils/timeseries"
 	"github.com/shpandrak/shpanstream/utils/timeseries/tsquery"
-	"github.com/shpandrak/shpanstream/utils/timeseries/tsquery/report"
+	"github.com/shpandrak/shpanstream/utils/timeseries/tsquery/datasource"
 )
 
 func ParseDatasource(
 	pCtx *ParsingContext,
 	ds ApiQueryDatasource,
-) (report.DataSource, error) {
+) (datasource.DataSource, error) {
 	rawDs, err := ds.ValueByDiscriminator()
 	if err != nil {
 		return nil, err
 	}
 
 	switch typedDs := rawDs.(type) {
-	case ApiJoinQueryDatasource:
-		return parseJoinDatasource(pCtx, typedDs)
 	case ApiStaticQueryDatasource:
 		return parseStaticDatasource(typedDs)
 	case ApiFilteredQueryDatasource:
 		return parseFilteredDatasource(pCtx, typedDs)
+	case ApiReductionQueryDatasource:
+		return parseReductionDatasource(pCtx, typedDs)
 	default:
 		return util.WrapAndReturn(pCtx.ParseDatasource(pCtx, ds))("failed parsing datasource with plugin parser")
 	}
 }
 
-func parseJoinDatasource(
-	pCtx *ParsingContext,
-	ds ApiJoinQueryDatasource,
-) (report.DataSource, error) {
-	multiDatasource, err := parseMultiDatasource(pCtx, ds.Datasources)
+func parseStaticDatasource(ds ApiStaticQueryDatasource) (datasource.DataSource, error) {
+	// Parse field metadata (single field for datasource package)
+	fieldMeta, err := tsquery.NewFieldMetaWithCustomData(
+		ds.FieldMeta.Uri,
+		ds.FieldMeta.DataType,
+		ds.FieldMeta.Required,
+		ds.FieldMeta.Unit,
+		ds.FieldMeta.CustomMetadata,
+	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse multi datasource for join: %w", err)
+		return nil, badInputErrorWrap(ds.FieldMeta, err, "failed to create field metadata for static datasource")
 	}
-	var joinType report.JoinType
-	switch ds.JoinType {
-	case Inner:
-		joinType = report.InnerJoin
-	case Left:
-		joinType = report.LeftJoin
-	case Full:
-		joinType = report.FullJoin
-	default:
-		return nil, fmt.Errorf("unsupported join type %v", ds.JoinType)
-	}
-	return report.NewJoinDatasource(multiDatasource, joinType), nil
 
-}
-
-func parseStaticDatasource(ds ApiStaticQueryDatasource) (report.DataSource, error) {
-	// Parse field metadata
-	var fieldsMeta []tsquery.FieldMeta
-	for _, apiMeta := range ds.FieldsMeta {
-		meta, err := tsquery.NewFieldMetaWithCustomData(
-			apiMeta.Uri,
-			apiMeta.DataType,
-			apiMeta.Required,
-			apiMeta.Unit,
-			apiMeta.CustomMetadata,
-		)
-		if err != nil {
-			return nil, badInputErrorWrap(apiMeta, err, "failed to create field metadata for static datasource")
+	// Convert API data to timeseries records (single value per record)
+	records := make([]timeseries.TsRecord[any], len(ds.Data))
+	for i, measurement := range ds.Data {
+		records[i] = timeseries.TsRecord[any]{
+			Timestamp: measurement.Timestamp,
+			Value:     measurement.Value,
 		}
-		fieldsMeta = append(fieldsMeta, *meta)
-	}
-
-	// Convert API data rows to timeseries records
-	var records []timeseries.TsRecord[[]any]
-	for _, row := range ds.Data {
-		records = append(records, timeseries.TsRecord[[]any]{
-			Timestamp: row.Timestamp,
-			Value:     row.Values,
-		})
 	}
 
 	// Create a stream from records
 	recordStream := stream.FromSlice(records)
 
 	// Create and return the static datasource
-	return report.NewStaticDatasource(fieldsMeta, recordStream)
+	return datasource.NewStaticDatasource(*fieldMeta, recordStream)
 }
 
-func parseMultiDatasource(pCtx *ParsingContext, multiDs ApiMultiDatasource) (report.MultiDataSource, error) {
+func parseMultiDatasource(pCtx *ParsingContext, multiDs ApiMultiDatasource) (datasource.MultiDataSource, error) {
 	valueByDiscriminator, err := multiDs.ValueByDiscriminator()
 	if err != nil {
 		return nil, err
@@ -96,27 +69,53 @@ func parseMultiDatasource(pCtx *ParsingContext, multiDs ApiMultiDatasource) (rep
 		return parseListMultiDatasource(pCtx, typedMds)
 	default:
 		return util.WrapAndReturn(pCtx.ParseMultiDatasource(pCtx, multiDs))("failed parsing multi datasource with plugin parser")
-
 	}
 }
 
-func parseListMultiDatasource(pCtx *ParsingContext, typedMds ApiListMultiDatasource) (report.MultiDataSource, error) {
-	dsList := make([]report.DataSource, len(typedMds.Datasources))
+func parseListMultiDatasource(pCtx *ParsingContext, typedMds ApiListMultiDatasource) (datasource.MultiDataSource, error) {
+	dsList := make([]datasource.DataSource, len(typedMds.Datasources))
 	for i, ds := range typedMds.Datasources {
-		filteredDatasource, err := ParseDatasource(pCtx, ds)
+		parsedDatasource, err := ParseDatasource(pCtx, ds)
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse datasource %d in list multi datasource: %w", i, err)
 		}
-		dsList[i] = filteredDatasource
-
+		dsList[i] = parsedDatasource
 	}
-	return report.NewListMultiDatasource(dsList), nil
+	return datasource.NewListMultiDatasource(dsList), nil
+}
+
+func parseReductionDatasource(
+	pCtx *ParsingContext,
+	reductionDs ApiReductionQueryDatasource,
+) (datasource.DataSource, error) {
+	// Parse alignment period
+	alignmentPeriod, err := parseAlignmentPeriod(reductionDs.AlignmentPeriod)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse alignment period for reduction datasource: %w", err)
+	}
+
+	// Parse multi datasource
+	multiDatasource, err := parseMultiDatasource(pCtx, reductionDs.MultiDatasource)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse multi datasource for reduction: %w", err)
+	}
+
+	// Parse field metadata
+	addFieldMeta := parseAddFieldMeta(reductionDs.FieldMeta)
+
+	// Create and return the reduction datasource
+	return datasource.NewReductionDatasource(
+		reductionDs.ReductionType,
+		alignmentPeriod,
+		multiDatasource,
+		addFieldMeta,
+	), nil
 }
 
 func parseFilteredDatasource(
 	pCtx *ParsingContext,
 	filteredDs ApiFilteredQueryDatasource,
-) (report.DataSource, error) {
+) (datasource.DataSource, error) {
 	unfilteredDatasource, err := ParseDatasource(pCtx, filteredDs.Datasource)
 	if err != nil {
 		return nil, err
@@ -124,7 +123,7 @@ func parseFilteredDatasource(
 	if len(filteredDs.Filters) == 0 {
 		return unfilteredDatasource, nil
 	}
-	var parsedFilters []report.Filter
+	var parsedFilters []datasource.Filter
 	for _, rawFilter := range filteredDs.Filters {
 		parsedFilter, err := ParseFilter(rawFilter)
 		if err != nil {
@@ -132,6 +131,5 @@ func parseFilteredDatasource(
 		}
 		parsedFilters = append(parsedFilters, parsedFilter)
 	}
-	return report.NewFilteredDataSource(unfilteredDatasource, parsedFilters...), nil
-
+	return datasource.NewFilteredDataSource(unfilteredDatasource, parsedFilters...), nil
 }
